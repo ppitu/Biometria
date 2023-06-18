adf427faf1ad7a78daf13f76ed3f39d7a0f0e6bf
Piotrek <piotr.chmura.1998@o2.pl>
2023-06-18 22:23:06 +0200

    Finish project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/core.hpp"
 #include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 
 #include <iostream>
@@ -48,15 +49,9 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
 
 int main(int argc, const char *argv[]) {
 	
-	if (argc < 3) {
-		cout << "usage: " << argv[0] << " <csv.ext> <output_folder> <test_image>" << endl;
+	if (argc < 2) {
+		cout << "usage: " << argv[0] << " <csv.ext> <test_image>" << endl;
 		exit(1);
-	}
-
-	string output_folder = ".";
-	
-	if (argc == 4) {
-		output_folder = string(argv[2]);
 	}
 
 	string fn_csv = string(argv[1]);
@@ -76,62 +71,21 @@ int main(int argc, const char *argv[]) {
 	}
 
 	int height = images[0].rows;
-	//Mat testSample = images[images.size() - 1];
-	//int testLabel = labels[labels.size() - 1];
-	
-	//images.pop_back();
-	//labels.pop_back();
+
 	Ptr<FisherFaceRecognizer> model = FisherFaceRecognizer::create();
 	
 	model->train(images, labels);
+	string testImagePath = string(argv[2]);
 	
-	int predictedLabel = model->predict(testSample);
+	auto testImage = imread(testImagePath, 0);
+
+	int pred = 0;
+	double conf = 0.0;
 	
-	string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, testLabel);
+	model->predict(testImage, pred, conf);
+
+	string result_message = format("Pred class = %d, loss = %f", pred, conf);
 	cout << result_message << endl;
-
-	
-	
-	//std::string str = format("Class: %d", predictedLabel);
-	
-	//cv::putText(testSample, str, cv::Point(10, testSample.rows / 2), cv::FONT_HERSHEY_DUPLEX, 0.5, CV_RGB(118, 185, 0), 2);
-	//cv::imwrite(format("%s/predic.png", output_folder.c_str()), testSample);
-	
-	/*Mat eigenvalues = model->getEigenValues();
-	Mat W = model->getEigenVectors();
-	Mat mean = model->getMean();
-	
-	if(argc == 2) {
-		imshow("mean", norm_0_255(mean.reshape(1, images[0].rows)));
-	} else {
-		imwrite(format("%s/mean.png", output_folder.c_str()), norm_0_255(mean.reshape(1, images[0].rows)));
-	}
-	
-	for (int i = 0; i < min(16, W.cols); i++) {
-		string msg = format("Eigenvalue #%d = %.5f", i, eigenvalues.at<double>(i));
-		cout << msg << endl;
-		Mat ev = W.col(i).clone();
-		Mat grayscale = norm_0_255(ev.reshape(1, height));
-		Mat cgrayscale;
-		applyColorMap(grayscale, cgrayscale, COLORMAP_BONE);
-		if(argc == 2) {
-			imshow(format("fisherface_%d", i), cgrayscale);
-		} else {
-			imwrite(format("%s/fisherface_%d.png", output_folder.c_str(), i), norm_0_255(cgrayscale));
-		}
-	}
-
-	for(int num_component = 0; num_component < min(16, W.cols); num_component++) {
-		Mat ev = W.col(num_component);
-		Mat projection = LDA::subspaceProject(ev, mean, images[0].reshape(1,1));
-		Mat reconstruction = LDA::subspaceReconstruct(ev, mean, projection);
-		reconstruction = norm_0_255(reconstruction.reshape(1, images[0].rows));
-		if(argc == 2) {
-			imshow(format("fisherface_reconstruction_%d", num_component), reconstruction);
-		} else {
-			imwrite(format("%s/fisherface_reconstruction_%d.png", output_folder.c_str(), num_component), reconstruction);
-		}
-		}*/
 
 	return 0;
 }
